77b3fe75a09bc40c764a2a7f0728a79094637e93
lindseyc03 <lindsey.cr123@gmail.com>
2019-02-16 16:59:28 +0000

    form

--- warmupDay2.py
+++ warmupDay2.py
@@ -1,2 +1,9 @@
 # file name: warmupDay2.py
 print(3)
+#form
+name = print("What is your name?")
+birthday = print(" What is your date of birth?")
+grade = print("What grade are you in")
+school = print("What school do you go to?")
+favSubject = print("What is your favorite subject in school?")
+growUp = print("What do you want to do when you grow up?")
