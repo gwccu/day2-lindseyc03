0f919d074790d6250ad886d05b5b7b4ec848a9e3
lindseyc03 <lindsey.cr123@gmail.com>
2019-02-16 17:26:18 +0000

    worked on form

--- problemSetDay2.py
+++ problemSetDay2.py
@@ -1,1 +1,15 @@
 # file name: problemSetDay2.py
+#form
+Name = input("What is your name?")
+birthday = input("What is your date of birth?")
+grade = input("What grade are you in?")
+school = input("What school do you go to?")
+favSubject = input("What is your favorite subject in school?")
+growUp = input("What do you want to do when you grow up?")
+
+fName=print("Your name is"+ Name)
+fbirthday=print("Your birthday is on"+ birthday)
+fgrade=print("You are in"+ grade)
+fschool=print("You go to"+ school)
+ffavsubject=print("Your favorite subject is"+ favSubject)
+fgrowUp=print("When you grow up you want to do "+ growUp)
